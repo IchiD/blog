54157aef6e9e0a1c390b49558d309c2084f1a568
IchiD <bigpoem33@gmail.com>
2023-08-18 12:45:07 +0900

    更新

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,19 +1,20 @@
 <nav class="bg-white border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 items-center">
-            <h1><a class="navbar-brand display-3" style="font-size: 3rem;" href="/">ブログ</a></h1>
+            <h1><a class="navbar-brand display-3" style="font-size: 3rem;" href="/">mブログ</a></h1>
 
             <div class="flex items-center">
+                <p class="px-3 py-2 fw-bold">{{ Auth::user()->name }}さん</p>
                 @if(url()->current() !== url('/'))
-                <a href="/" class="text-gray-500 hover:text-gray-700 px-3 py-2">ホーム</a>
+                <a href="/" class="px-3 py-2">ホーム</a>
                 @endif
                 @if(url()->current() !== url('/profile'))
-                <a href="{{ route('profile.edit') }}" class="text-gray-500 hover:text-gray-700 px-3 py-2">プロフィール</a>
+                <a href="{{ route('profile.edit') }}" class="px-3 py-2">プロフィール</a>
                 @endif
                 @if(Auth::check())
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="text-gray-500 hover:text-gray-700 px-3 py-2">ログアウト</a>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="px-3 py-2">ログアウト</a>
                 </form>
                 @else
                 <a href="{{ route('login') }}" class="text-gray-500 hover:text-gray-700 px-3 py-2">ログイン</a>
